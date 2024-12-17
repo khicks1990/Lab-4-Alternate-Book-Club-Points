87e7e9725fe8c6bfb6d72bd117cdc64a340dbb85
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 07:34:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+   This program demonstrates a solution to the
+   Book Club Points programming challenge.
+*/
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Variables
+
+      // Create a Scanner object for keyboard input.
+
+      // Get the number of books purchased this month.
+      
+      // Determine the number of points to award.
+
+         
+      // Display the points earned.
+ 
+   }
+}
